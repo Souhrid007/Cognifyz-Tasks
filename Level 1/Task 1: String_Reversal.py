5c5bf7904a8844cb6995f2ec3ed792f132569d0a
Souhrid Suresh <98485867+Souhrid007@users.noreply.github.com>
2024-07-12 15:44:20 +0530

    Create Task 1: String_Reversal.py

---
+++ Level 1/Task 1: String_Reversal.py
@@ -0,0 +1,6 @@
+def reverse_string():
+    input_string = input("Enter a string: ")
+    return input_string[::-1]
+    
+reversed_string = reverse_string()
+print("Reversed string:", reversed_string)
